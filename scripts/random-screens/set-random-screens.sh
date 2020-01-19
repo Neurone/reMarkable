08dd44365dd38d9bd3d4048e75803b53fb375034
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2020-01-19 03:24:15 +0100

    Change poweroff and suspended screen every 5 minutes;

---
+++ scripts/random-screens/set-random-screens.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+IMAGES_DIR=/home/root/customization/images
+
+POWEROFF_IMAGES_DIR=$IMAGES_DIR/poweroff
+POWEROFF_FILE=/usr/share/remarkable/poweroff.original.png
+if [ -d $POWEROFF_IMAGES_DIR ]; then
+    POWEROFF_IMAGES_COUNT=$(ls $POWEROFF_IMAGES_DIR | wc -l)
+    if [ $POWEROFF_IMAGES_COUNT -ne 0 ]; then
+        POWEROFF_FILE=$(shuf -n1 -e $POWEROFF_IMAGES_DIR/*)
+    fi;
+fi
+ln -sf $POWEROFF_FILE /usr/share/remarkable/poweroff.png
+
+SUSPENDED_IMAGES_DIR=$IMAGES_DIR/suspended
+SUSPENDED_FILE=/usr/share/remarkable/suspended.original.png
+if [ -d $SUSPENDED_IMAGES_DIR ]; then
+    SUSPENDED_IMAGES_COUNT=$(ls $SUSPENDED_IMAGES_DIR | wc -l)
+    if [ $SUSPENDED_IMAGES_COUNT -ne 0 ]; then
+        SUSPENDED_FILE=$(shuf -n1 -e $SUSPENDED_IMAGES_DIR/*)
+    fi;
+fi
+ln -sf $SUSPENDED_FILE /usr/share/remarkable/suspended.png
